6b9422ca685ef19a5559a238cca07a92b58ecca0
tamasbrandstadter <tamas.brandstadter@gmail.com>
2017-10-11 19:17:15 +0200

    add solution

--- LeetCode/src/Main.java
+++ LeetCode/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
   public static void main(String[] args) {
 
@@ -5,7 +8,7 @@ public class Main {
 
   //Given an array of integers, return indices of the two numbers such that they add up to a specific target.
   //You may assume that each input would have exactly one solution, and you may not use the same element twice.
-  public static int[] twoSum(int[] nums, int target) {
+  private static int[] twoSum(int[] nums, int target) {
     for (int i = 0; i < nums.length; i++) {
       for (int j = 1; j >= i; j++) {
         if (nums[i] + nums[j] == target) {
@@ -18,7 +21,7 @@ public class Main {
 
   //Reverse digits of an integer.
   //The input is assumed to be a 32-bit signed integer. Your function should return 0 when the reversed integer overflows.
-  public static int reverseInteger(int x) {
+  private static int reverseInteger(int x) {
     long reverse = 0;
     while (x != 0) {
       reverse = reverse * 10 + x % 10;
@@ -32,7 +35,7 @@ public class Main {
   }
 
   //Determine whether an integer is a palindrome.
-  public static boolean isPalindrome(int x) {
+  private static boolean isPalindrome(int x) {
     if (String.valueOf(x).startsWith("-")) {
       x -= x + x;
     }
@@ -40,7 +43,33 @@ public class Main {
   }
 
   //Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
-  public static int strStr(String haystack, String needle) {
+  private static int strStr(String haystack, String needle) {
     return haystack.contains(needle) ? haystack.indexOf(needle) : -1;
+  }
+
+  //Given a roman numeral, convert it to an integer.
+  private static int romanToInt(String s) {
+    Map<Character, Integer> romanNumberMap = new HashMap<>();
+    romanNumberMap.put('I', 1);
+    romanNumberMap.put('V', 5);
+    romanNumberMap.put('X', 10);
+    romanNumberMap.put('L', 50);
+    romanNumberMap.put('C', 100);
+    romanNumberMap.put('D', 500);
+    romanNumberMap.put('M', 1000);
+
+    int num = 0;
+    int prev = 0;
+    for (int i = s.length()-1; i >= 0; i--) {
+      int temp = romanNumberMap.get(s.charAt(i));
+      if (prev <= temp) {
+        num += temp;
+        prev = temp;
+      } else {
+        num -= temp;
+        prev = temp;
+      }
+    }
+    return num;
   }
 }
