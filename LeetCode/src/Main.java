2a82f90cdd0c9cb6ebcf21812a38771d6fbb31a3
tamasbrandstadter <tamas.brandstadter@gmail.com>
2017-10-22 16:22:49 +0200

    add solution for arrangeCoins

--- LeetCode/src/Main.java
+++ LeetCode/src/Main.java
@@ -253,5 +253,13 @@ public class Main {
 
     return root;
   }
+
+  //You have a total of n coins that you want to form in a staircase shape, where every k-th row must have exactly k coins.
+  //Given n, find the total number of full staircase rows that can be formed.
+  //n is a non-negative integer and fits within the range of a 32-bit signed integer.
+  private static int arrangeCoins(int n) {
+    return (int) ((Math.sqrt(1 + 8.0 * n) - 1) / 2);
+  }
+
 }
 
