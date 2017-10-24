cef07d10191c756e1c478b8aa7fa5282c0e23b42
tamasbrandstadter <tamas.brandstadter@gmail.com>
2017-10-24 19:47:55 +0200

    add solution for removing linked list elements

--- LeetCode/src/Main.java
+++ LeetCode/src/Main.java
@@ -451,6 +451,24 @@ public class Main {
     return reverseListInt(next, head);
   }
 
+  //Remove all elements from a linked list of integers that have value val.
+  private static ListNode removeElements(ListNode head, int val) {
+    if (head == null) {
+      return null;
+    }
+
+    ListNode pointer = head;
+
+    while (pointer.next != null) {
+      if (pointer.next.val == val) {
+        pointer.next = pointer.next.next;
+      } else {
+        pointer = pointer.next;
+      }
+    }
+    return head.val == val ? head.next : head;
+  }
+
   //Write a function that takes a string as input and returns the string reversed.
   private static String reverseString(String s) {
     return new StringBuilder(s).reverse().toString();
