dd78e32b7765346333ac3833059f0c7f619862ca
tamasbrandstadter <tamas.brandstadter@gmail.com>
2017-11-26 12:03:51 +0100

    add solution for detectCapitalUse

--- LeetCode/src/Main.java
+++ LeetCode/src/Main.java
@@ -687,4 +687,14 @@ public class Main {
     return sb.toString();
   }
 
+  /*Given a word, you need to judge whether the usage of capitals in it is right or not.
+  We define the usage of capitals in a word to be right when one of the following cases holds:
+  All letters in this word are capitals, like "USA".
+  All letters in this word are not capitals, like "leetcode".
+  Only the first letter in this word is capital if it has more than one letter, like "Google".
+  Otherwise, we define that this word doesn't use capitals in a right way.*/
+  private static boolean detectCapitalUse(String word) {
+    return word.matches("^[A-Z][A-Z]*[A-Z]*$") || word.matches("^[A-Z][a-z]*[a-z]$") || word.matches("^[a-z][a-z]*[a-z]*$");
+  }
+
 }
