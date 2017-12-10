7ab497c4109d9c3772753fd0cf14344822f05ef4
tamasbrandstadter <tamas.brandstadter@gmail.com>
2017-12-10 21:35:59 +0100

    add solution for plusOne

--- LeetCode/src/Main.java
+++ LeetCode/src/Main.java
@@ -15,6 +15,7 @@ import static java.util.stream.LongStream.rangeClosed;
 
 public class Main {
   public static void main(String[] args) {
+    System.out.println(Arrays.toString(plusOne(new int[] {9, 9})));
   }
 
   //Given an array of integers, return indices of the two numbers such that they add up to a specific target.
@@ -795,6 +796,24 @@ public class Main {
     }
 
     return nums[nums.length - 1];
+  }
+
+  /*Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
+  You may assume the integer do not contain any leading zero, except the number 0 itself.
+  The digits are stored such that the most significant digit is at the head of the list.*/
+  private static int[] plusOne(int[] digits) {
+    for (int i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] != 9) {
+        digits[i]++;
+        return digits;
+      }
+      digits[i] = 0;
+    }
+
+    int[] nums = new int[digits.length + 1];
+    nums[0] = 1;
+
+    return nums;
   }
 
 }
