a61dc2361e9282bcc52938a900a286bcc60575d4
tamasbrandstadter <tamas.brandstadter@gmail.com>
2017-12-28 12:08:07 +0100

    add implementation for StackWithQueue class

---
+++ LeetCode/src/StackWithQueue.java
@@ -0,0 +1,33 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class StackWithQueue {
+  private Deque<Integer> queue;
+
+  public StackWithQueue() {
+    this.queue = new LinkedList<>();
+  }
+
+  public void push(int x) {
+    queue.addFirst(x);
+  }
+
+  public int pop() {
+    if (!queue.isEmpty()) {
+      return queue.pollFirst();
+    }
+    return -1;
+  }
+
+  public int top() {
+    if (!queue.isEmpty()) {
+      return queue.peekFirst();
+    }
+    return -1;
+  }
+
+  public boolean empty() {
+    return queue.isEmpty();
+  }
+
+}
