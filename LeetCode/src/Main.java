fc6bd4e5de00663e13b8bdbff91febdb51185a32
tamasbrandstadter <tamas.brandstadter@gmail.com>
2017-12-30 11:27:22 +0100

    add description to exercise

--- LeetCode/src/Main.java
+++ LeetCode/src/Main.java
@@ -12,7 +12,6 @@ import java.util.stream.LongStream;
 
 public class Main {
   public static void main(String[] args) {
-   findBadVersionBinary(5);
   }
 
   //Given an array of integers, return indices of the two numbers such that they add up to a specific target.
@@ -1038,6 +1037,11 @@ public class Main {
     return res;
   }
 
+  /*You are a product manager and currently leading a team to develop a new product.
+  Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version,
+  all the versions after a bad version are also bad. Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one,
+  which causes all the following ones to be bad. You are given an API bool isBadVersion(version) which will return whether version is bad
+  Implement a function to find the first bad version. You should minimize the number of calls to the API.*/
   private static int firstBadVersionRecursively(int n) {
     if (isBadVersion(n)) {
       return n;
