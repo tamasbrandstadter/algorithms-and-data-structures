1ef7dfd6f6991008de5abf47076ce46463f9d436
tamasbrandstadter <tamas.brandstadter@gmail.com>
2017-12-31 11:19:59 +0100

    minor refactor

--- LeetCode/src/Graph.java
+++ LeetCode/src/Graph.java
@@ -1,13 +1,15 @@
+import java.util.stream.IntStream;
+
 public class Graph {
-  private Vertex[] adjacents;
+  private Vertex[] vertices;
   private int index;
-  private boolean isDirected;
   private int vertexCount;
+  private boolean isDirected;
 
   public Graph(int vertexCount, boolean isDirected) {
     this.vertexCount = vertexCount;
     this.isDirected = isDirected;
-    this.adjacents = new Vertex[vertexCount];
+    this.vertices = new Vertex[vertexCount];
     this.index = 0;
   }
 
@@ -15,7 +17,7 @@ public class Graph {
     if (index == vertexCount) {
       System.out.println("Graph is full, cannot add more vertices.");
     } else {
-      this.adjacents[index++] = new Vertex(name, null);
+      this.vertices[index++] = new Vertex(name, null);
     }
   }
 
@@ -23,28 +25,26 @@ public class Graph {
     int sourceIndex = getIndexByName(sourceVertex);
     int destIndex = getIndexByName(destinationVertex);
 
-    this.adjacents[sourceIndex].setAdjacent(new ListNode(destIndex, adjacents[sourceIndex].getAdjacent()));
+    this.vertices[sourceIndex].setAdjacent(new ListNode(destIndex, vertices[sourceIndex].getAdjacent()));
     if (!isDirected) {
-      this.adjacents[destIndex].setAdjacent(new ListNode(sourceIndex, adjacents[destIndex].getAdjacent()));
+      this.vertices[destIndex].setAdjacent(new ListNode(sourceIndex, vertices[destIndex].getAdjacent()));
     }
   }
 
   private int getIndexByName(String name) {
-    for (int i = 0; i < adjacents.length; i++) {
-      if (adjacents[i].getName().equals(name)) {
-        return i;
-      }
-    }
-    return -1;
+    return IntStream.range(0, vertices.length)
+        .filter(i -> vertices[i].getName().equals(name))
+        .findFirst()
+        .orElse(-1);
   }
 
   public void printGraph() {
     int i = 0;
-    while (i < adjacents.length) {
-      System.out.print(adjacents[i].getName());
-      ListNode adjacent = adjacents[i].getAdjacent();
+    while (i < vertices.length) {
+      System.out.print(vertices[i].getName());
+      ListNode adjacent = vertices[i].getAdjacent();
       while (adjacent != null) {
-        System.out.print(" ---> " + adjacents[adjacent.val].getName());
+        System.out.print(" ---> " + vertices[adjacent.val].getName());
         adjacent = adjacent.next;
       }
       System.out.println();
