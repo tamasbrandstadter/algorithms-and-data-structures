f77e76d16f11f43c5842d8e41d87a5400f2146a9
tamasbrandstadter <tamas.brandstadter@gmail.com>
2018-01-31 21:28:56 +0100

    add solution for moveZeroes

--- LeetCode/src/Main.java
+++ LeetCode/src/Main.java
@@ -1421,4 +1421,21 @@ public class Main {
     return results;
   }
 
+  /*Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+  For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+  You must do this in-place without making a copy of the array. Minimize the total number of operations.*/
+  private static void moveZeroes(int[] nums) {
+    int nonZeroElementCount = 0;
+
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != 0) {
+        nums[nonZeroElementCount++] = nums[i];
+      }
+    }
+
+    for (int i = nonZeroElementCount; i < nums.length; i++) {
+      nums[i] = 0;
+    }
+  }
+
 }
