2ac699117b60957da2639e34577cdec7f7343313
tamasbrandstadter <tamas.brandstadter@gmail.com>
2018-02-04 20:50:36 +0100

    add solution for arrayPairSum

--- LeetCode/src/Main.java
+++ LeetCode/src/Main.java
@@ -1438,4 +1438,25 @@ public class Main {
     }
   }
 
+  //Given an array of 2n integers, your task is to group these integers into n pairs of integer, say (a1, b1), (a2, b2), ..., (an, bn)
+  //which makes sum of min(ai, bi) for all i from 1 to n as large as possible.
+  private static int arrayPairSumStream(int[] nums) {
+    Arrays.sort(nums);
+
+    return IntStream.iterate(0, i -> i <= nums.length - 1, i -> i + 2)
+        .map(i -> nums[i])
+        .sum();
+  }
+
+  private static int arrayPairSumIterative(int[] nums) {
+    Arrays.sort(nums);
+
+    int sum = 0;
+    for (int i = 0; i <= nums.length - 1; i += 2) {
+      sum += nums[i];
+    }
+
+    return sum;
+  }
+
 }
