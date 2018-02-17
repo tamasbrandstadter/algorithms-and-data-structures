7e5395df12708b394f0aab4c92258417db24fe32
tamasbrandstadter <tamas.brandstadter@gmail.com>
2018-02-17 15:05:41 +0100

    add solution for Percolation at course of Princeton Algorithms Part 1 week 1 assignment

---
+++ PrincetonAlgorithms/Part1/Percolation/src/Percolation.java
@@ -0,0 +1,78 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+    private int openSites;
+    private int size;
+    private WeightedQuickUnionUF uf;
+    private boolean[][] grid;
+
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Grid must be at least 1x1");
+        }
+
+        this.uf = new WeightedQuickUnionUF(n * n + 2);
+        this.openSites = 0;
+        this.size = n;
+        this.grid = new boolean[n][n];
+
+        for (int i = 1; i <= grid.length; i++) {
+            uf.union(0, convertCoordinatesToId(1, i));
+            uf.union(n * n + 1, convertCoordinatesToId(n, i));
+        }
+    }
+
+    public void open(int row, int col) {
+        if (size < row || row < 1 || col < 1 || col > size) {
+            throw new IllegalArgumentException("Index must be in bounds. Min index is 1, max index is " + size);
+        }
+
+        openSites++;
+
+        int current = convertCoordinatesToId(row, col);
+
+        grid[row - 1][col - 1] = true;
+
+        if (col > 1 && isOpen(row, col - 1)) {
+            uf.union(current, convertCoordinatesToId(row, col - 1));
+        }
+
+        if (row < size && isOpen(row + 1, col)) {
+            uf.union(current, convertCoordinatesToId(row + 1, col));
+        }
+
+        if (col < size && isOpen(row, col + 1)) {
+            uf.union(current, convertCoordinatesToId(row, col + 1));
+        }
+
+        if (row > 1 && isOpen(row - 1, col)) {
+            uf.union(current, convertCoordinatesToId(row - 1, col));
+        }
+
+    }
+
+    public boolean isOpen(int row, int col) {
+        return grid[row - 1][col - 1];
+    }
+
+    public boolean isFull(int row, int col) {
+        return isOpen(row, col) && uf.connected(convertCoordinatesToId(row, col), 0);
+    }
+
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    public boolean percolates() {
+        return uf.connected(0, size * size + 1);
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    private int convertCoordinatesToId(int row, int column) {
+        return (row - 1) * grid.length + column;
+    }
+
+}
---
+++ PrincetonAlgorithms/Part1/Percolation/src/PercolationStats.java
@@ -0,0 +1,54 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+    private Percolation percolation;
+    private double[] tresholds;
+
+    public PercolationStats(int n, int trials) {
+        if (n <= 0 || trials <= 0) {
+            throw new IllegalArgumentException("N and trials must be greater than 0");
+        }
+
+        this.tresholds = new double[trials];
+
+        for (int i = 0; i < trials; i++) {
+            this.percolation = new Percolation(n);
+            while (!percolation.percolates()) {
+                int uniform = StdRandom.uniform(n) + 1;
+                int uniform2 = StdRandom.uniform(n) + 1;
+                if (!percolation.isOpen(uniform, uniform2)) {
+                    percolation.open(uniform, uniform2);
+                }
+            }
+            tresholds[i] = (double) percolation.numberOfOpenSites() / (n * n);
+        }
+    }
+
+    public double mean() {
+        return StdStats.mean(tresholds);
+    }
+
+    public double stddev() {
+        return StdStats.stddev(tresholds);
+    }
+
+    public double confidenceLo() {
+        return mean() - 1.96 * stddev() / Math.sqrt(tresholds.length);
+    }
+
+    public double confidenceHi() {
+        return mean() + 1.96 * stddev() / Math.sqrt(tresholds.length);
+    }
+
+    public static void main(String[] args) {
+        PercolationStats percolationStats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+
+        StdOut.println("mean = " + percolationStats.mean());
+        StdOut.println("stddev = " + percolationStats.stddev());
+
+        StdOut.printf("95%% confidence interval = [ %f , %f ]", percolationStats.confidenceLo(), percolationStats.confidenceHi());
+    }
+
+}
