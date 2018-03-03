c02c0c27021cd2ba26b09900930df47b25b23bce
tamasbrandstadter <tamas.brandstadter@gmail.com>
2018-03-03 21:08:55 +0100

    add implementation for NodeStack

---
+++ LeetCode/src/holczer/udemy/NodeStack.java
@@ -0,0 +1,59 @@
+package holczer.udemy;
+
+import java.util.EmptyStackException;
+
+public class NodeStack<T extends Comparable<T>> {
+  private Node<T> root;
+  private int size;
+
+  public void push(T data) {
+    size++;
+    if (root == null) {
+      this.root = new Node<>(data);
+    } else {
+      Node<T> oldRoot = root;
+      root = new Node<>(data);
+      root.setNext(oldRoot);
+    }
+  }
+
+  public T pop() {
+    if (!isEmpty()) {
+      size--;
+      T data = root.getData();
+      root = root.getNext();
+      return data;
+    } else {
+      throw new EmptyStackException();
+    }
+
+  }
+
+  public T peek() {
+    if (!isEmpty()) {
+      return root.getData();
+    } else {
+      throw new EmptyStackException();
+    }
+  }
+
+  public boolean isEmpty() {
+    return root == null && size == 0;
+  }
+
+  public int size() {
+    return size;
+  }
+
+  public void print() {
+    if (!isEmpty()) {
+      Node<T> current = root;
+      while (current != null) {
+        System.out.println(current.toString());
+      }
+    } else {
+      throw new EmptyStackException();
+    }
+  }
+
+}
